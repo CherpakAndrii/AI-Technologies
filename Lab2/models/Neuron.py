cdcb1a515ba81476bff6cdbda5c06d4213395909
CherpakAndrii <a.cherpak-2026@kpi.ua>
2024-04-01 03:13:48 +0300

    naming corrected

--- Lab2/models/Neuron.py
+++ Lab2/models/Neuron.py
@@ -4,19 +4,19 @@ from models.Synapse import Synapse
 
 
 class Neuron(INeuron):
-    weight: float
+    bias: float
     inputs: list[Synapse]
     activation_func: IActivationFunction
     out_value: float|None
 
-    def __init__(self, input_synapses: list[Synapse], weight: float, activation_function: IActivationFunction):
+    def __init__(self, input_synapses: list[Synapse], bias: float, activation_function: IActivationFunction):
         self.inputs = input_synapses
-        self.weight = weight
+        self.bias = bias
         self.activation_func = activation_function
 
     def feedforward(self):
         signals_sum = sum([s.get_signal() for s in self.inputs])
-        self.out_value = self.activation_func.compute(signals_sum + self.weight)
+        self.out_value = self.activation_func.compute(signals_sum + self.bias)
 
     def get_value(self) -> float|None:
         value = self.out_value
